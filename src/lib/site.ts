021523820fea93c05c2b9ea5260ea481e323f34b
Techonni <20517985+techonni@users.noreply.github.com>
2026-09-17 12:02:19 -0700

    Point X links to @techonni

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -8,11 +8,10 @@ export const SITE = {
   site: 'https://fx.techonni.com',
   base: '/',
   telegram: 'https://t.me/EuroDeskFX',
-  x: 'https://x.com/eurodeskfx',
-  xHandle: '@eurodeskfx',
+  x: 'https://x.com/techonni',
+  xHandle: '@techonni',
   pairs: ['EURUSD', 'GBPUSD'] as const,
   locale: 'en',
-  // Google Form formResponse URL (filled after form is created)
   newsletterFormAction: 'https://docs.google.com/forms/d/e/1FAIpQLSfs9xCOoFKH5zT9z2cmgvOtsRjE59axfKI_EtYDpx55DtoiHQ/formResponse',
   newsletterNameEntry: 'entry.1110274731',
   newsletterEmailEntry: 'entry.1946205931',
