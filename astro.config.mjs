91ea03514b5536830a3e7dda59835650e038161e
techonni <techonni@users.noreply.github.com>
2026-09-17 12:14:03 +0000

    Align astro site URL with fx.techonni.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.techonni.com',
+  site: 'https://fx.techonni.com',
   base: '/',
   output: 'static',
   trailingSlash: 'always',
